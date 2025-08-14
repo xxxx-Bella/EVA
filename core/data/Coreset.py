b1e9519224b3b4d9baa2781c49cdf49b13785341
xxxx-Bella <73006896+xxxx-Bella@users.noreply.github.com>
2025-08-14 17:40:16 +0800

    rm unused parameters

--- core/data/Coreset.py
+++ core/data/Coreset.py
@@ -7,33 +7,9 @@ class CoresetSelection(object):
         score_sorted_index = score.argsort(descending=descending)
         total_num = ratio * data_score['targets'].shape[0]
 
-        if class_balanced:
-            print('Class balance mode.')
-            all_index = torch.arange(data_score['targets'].shape[0])
-            #Permutation
-            targets_list = data_score['targets'][score_sorted_index]
-            targets_unique = torch.unique(targets_list)
-            for target in targets_unique:
-                target_index_mask = (targets_list == target)
-                targets_num = target_index_mask.sum()
-
-            #Guarantee the class ratio doesn't change
-            selected_index = []
-            for target in targets_unique:
-                target_index_mask = (targets_list == target)
-                target_index = all_index[target_index_mask]
-                target_coreset_num = targets_num * ratio
-                selected_index = selected_index + list(target_index[:int(target_coreset_num)])
-            selected_index = torch.tensor(selected_index)
-            print(f'High priority {key}: {score[score_sorted_index[selected_index][:15]]}')
-            print(f'Low priority {key}: {score[score_sorted_index[selected_index][-15:]]}')
-
-            return score_sorted_index[selected_index]
-
-        else:
-            print(f'High priority {key}: {score[score_sorted_index[:15]]}')
-            print(f'Low priority {key}: {score[score_sorted_index[-15:]]}')
-            return score_sorted_index[:int(total_num)]
+        print(f'High priority {key}: {score[score_sorted_index[:15]]}')
+        print(f'Low priority {key}: {score[score_sorted_index[-15:]]}')
+        return score_sorted_index[:int(total_num)]
 
     @staticmethod
     def mislabel_mask(data_score, mis_key, mis_num, mis_descending, coreset_key):
